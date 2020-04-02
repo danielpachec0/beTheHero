b09371e52806464ffe326c7067df8635a78da03a
danielpachec0 <danielazepac@gmail.com>
2020-04-02 20:48:32 -0300

    add system BackEnd

---
+++ back/src/routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const ongController = require('./Controllers/ongController');
+const incidentController = require('./Controllers/incidentController');
+const profileController = require('./Controllers/profileController');
+const sessionController = require('./Controllers/sessionController');
+
+
+const routes = express.Router();
+
+routes.get('/ongs', ongController.index);
+
+routes.post('/ongs', ongController.create);
+
+routes.get('/incidents', incidentController.index);
+
+routes.post('/incidents', incidentController.create);
+
+routes.delete('/incidents/:id', incidentController.delete);
+
+routes.get('/profile', profileController.index);
+
+routes.post('/sessions', sessionController.create)
+
+module.exports = routes;
